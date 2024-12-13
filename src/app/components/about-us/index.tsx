f563ca70de97bed6d621c3875f1a3f9cd12ae61e
hk674 <hk674@gmail.com>
2024-12-13 14:26:15 +0530

    upt

--- src/app/components/about-us/index.tsx
+++ src/app/components/about-us/index.tsx
@@ -1,36 +1,36 @@
+import Image from "next/image";
+
 export default function AboutUs() {
   return (
     <div id="about-us" className="bg-blue-950">
       <div className="mx-auto max-w-screen-2xl">
-        <div className="relative grid grid-cols-2 h-svh p-36 gap-20">
-          <div className="flex flex-col items-start">
-            <p className="text-7xl font-black">Who Are</p>
-            <p className="text-7xl font-extralight">We</p>
-            <p className="text-slate-500">
-              SHS is a water treatment company Established in the year 1998. SHS
-              is a water treatment company Established in the year 1998. SHS is
-              a water treatment company Established in the year 1998. SHS is a
-              water treatment company Established in the year 1998. SHS is a
-              water treatment company Established in the year 1998. SHS is a
-              water treatment company Established in the year 1998. SHS is a
-              water treatment company Established in the year 1998. SHS is a
-              water treatment company Established in the year 1998. SHS is a
-              water treatment company Established in the year 1998.
+        <div className="relative grid grid-cols-2 h-svh p-36 gap-36">
+          <div className="flex flex-col items-start justify-content-center pt-12">
+            <p className="text-md font-black mb-3">
+              Transforming industrial water treatment
+            </p>
+            <p className="text-3xl font-extralight mb-5">
+              INNOVATIVE WATER SOLUTIONS
+            </p>
+            <p className="text-md text-slate-500 tracking-wide">
+              At SHS, we specialize in advanced industrial water treatment
+              solutions designed to empower businesses and optimize their
+              operations. Based in [location], our dedicated team is committed
+              to delivering highly efficient and reliable water treatment
+              systems, meticulously tailored to meet the distinct needs of
+              diverse industries. We prioritize cutting-edge innovation and
+              strict compliance with regulatory standards, ensuring our clients
+              achieve peak performance while maintaining a strong commitment to
+              environmental sustainability. Partner with SHS to revolutionize
+              your water treatment processes and propel your success to new
+              heights.
             </p>
           </div>
-          <div className="flex flex-col items-center align-stretch">
-            <p className="text-3xl font-black mt-12">Vision</p>
-            <p className="text-slate-500">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-              architecto harum sed repudiandae cum libero sint in placeat
-              dolorem! Corporis veniam accusantium laudantium.
-            </p>
-            <p className="text-3xl font-black mt-24">Mission</p>
-            <p className="text-slate-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-              natus optio ducimus quidem, reprehenderit, modi facere inventore
-              quia repellendus quae quis eius provident?
-            </p>
+          <div
+            className="flex flex-col items-center justify-content-center"
+            style={{ position: "relative" }}
+          >
+            <Image src="/aboutUs.jpeg" alt="" fill />
           </div>
         </div>
       </div>
