6ee78d811d0d0489e3de663bb5697e36b2d451d3
hk674 <hk674@gmail.com>
2024-12-24 19:22:11 +0530

    upt

--- src/app/components/home/index.tsx
+++ src/app/components/home/index.tsx
@@ -11,6 +11,7 @@ export default function Home() {
             //   width={100}
             //   height={500}
             fill={true}
+            unoptimized
           />
           <div className="flex items-center justify-center absolute inset-0 z-10 text-white text-center font-black text-3xl ">
             Water Services from SHS with the Quote
