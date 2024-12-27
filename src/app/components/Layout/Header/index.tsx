88005fd3d944d342cf79673bd782a3e535f91048
hk674 <hk674@gmail.com>
2024-12-27 16:45:11 +0530

    ut

--- src/app/components/Layout/Header/index.tsx
+++ src/app/components/Layout/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <nav className="bg-yellow-50">
@@ -92,13 +94,13 @@ export default function Header() {
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                <a
+                <Link
                   href="/"
                   className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
                   aria-current="page"
                 >
                   Home
-                </a>
+                </Link>
                 <a
                   href="#about-us"
                   className="rounded-md px-3 py-2 text-sm font-medium text-blue-950 hover:bg-gray-700 hover:text-white"
